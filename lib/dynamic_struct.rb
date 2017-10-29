8886b7cdd8445a846e46bf48cf31febf2a4bf4c4
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-10-30 00:18:16 +0100

    Add and update gem version for 1.0.0

--- lib/dynamic_struct.rb
+++ lib/dynamic_struct.rb
@@ -1,5 +1,7 @@
 # DynamicStruct is data structure similar on OpenStruct,
 # but a little more optimized
 
+require_relative './dynamic_struct/version'
+
 module DynamicStruct
 end
--- lib/dynamic_struct/version.rb
+++ lib/dynamic_struct/version.rb
@@ -1,3 +1,3 @@
 module DynamicStruct
-  VERSION = "0.1.0"
+  VERSION = '1.0.0'.freeze
 end
