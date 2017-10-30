ef902f17f2ba437203f717e834197eb3b914561c
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-10-30 01:29:12 +0100

    Add specs for describing custom methods - DynamicStruct#build

--- spec/dynamic_struct_spec.rb
+++ spec/dynamic_struct_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe DynamicStruct do
       expect(subject.const_defined?(:Corpus)).to eq(true)
     end
   end
+
+
+  describe 'custom methods -' do
+    context '#build -' do
+      it 'expects subject respond to context' do
+        expect(subject.respond_to?(:build)).to be_truthy
+      end
+
+      it 'expects context to be a singleton method' do
+        expect(subject.singleton_class.method_defined?(:build)).to eq(true)
+      end
+
+      it 'expects context to return instance of DynamicStruct::Corpus' do
+        expect(subject.build.class).to be(DynamicStruct::Corpus)
+      end
+    end
+  end
 end
