b55271afb7fe2955da1c0b8951001437f8415b6a
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-10-30 01:27:00 +0100

    Add general specs for described class

--- spec/dynamic_struct_spec.rb
+++ spec/dynamic_struct_spec.rb
@@ -1,7 +1,13 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe DynamicStruct do
+  let(:subject) { described_class }
+
   it "has a version number" do
-    expect(DynamicStruct::VERSION).not_to be nil
+    expect(subject::VERSION).not_to be nil
+  end
+
+  it 'expects subject to be a module' do
+    expect(subject.class).to be(Module)
   end
 end
