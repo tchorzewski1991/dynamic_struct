38dfbd07f0e90592d5705a3d19875c30dc77abfe
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-10-31 01:02:09 +0100

    Implement logic for DynamicStruct::Corpus#inspect method
    -> final output is really similar to OpenStruct object

--- lib/dynamic_struct/corpus.rb
+++ lib/dynamic_struct/corpus.rb
@@ -4,6 +4,19 @@ module DynamicStruct
       @atoms = {}
     end
 
+    def inspect
+      klass = self.class
+
+      if atoms?
+        %Q(<%s %s>) % [
+          klass,
+          atoms.map { |k, v| "%s=%s" % [k, v.inspect] } * ' '
+        ]
+      else
+        %Q(<%s>) % klass
+      end
+    end
+
     def atoms?
       atoms.any?
     end
