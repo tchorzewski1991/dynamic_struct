7b7d3a2212467a2485b3001a96b95bf7e4d823c4
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-09 20:18:43 +0100

    Add description for general purpose and usage for call-inside matcher

--- spec/support/matchers/call_inside.rb
+++ spec/support/matchers/call_inside.rb
@@ -1,4 +1,28 @@
 RSpec::Matchers.define :call do |target|
+
+  # The main responsibility of #call(...).inside(...) matcher is to create
+  # flexible mechanism for observing 'target' method invocation during
+  # execution of 'host' method. There is a lot of metaprogramming in
+  # here but the point is to understand subject mocking and host mocking.
+
+  # Examples of usage
+  #
+  # When #another method has no arguments:
+  #
+  # it 'expects to call #one method inside #another method' do
+  #   is_expected.to call('one').inside('another')
+  # end
+  #
+  # When #another method expects arguments:
+  #
+  # it 'expects to call #one method inside #another method' do
+  #   is_expected.to(
+  #     call('one').inside('another') do
+  #       |host| host.argument = argument
+  #     end
+  #   )
+  # end
+
   match do |subject|
   end
 
