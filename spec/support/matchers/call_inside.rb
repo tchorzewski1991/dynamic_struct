5de9e1bff35bc984a01d90bd9203ed8a9e8381f1
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-09 20:28:13 +0100

    Implement core 'matching' logic and chaining rule

--- spec/support/matchers/call_inside.rb
+++ spec/support/matchers/call_inside.rb
@@ -24,8 +24,20 @@ RSpec::Matchers.define :call do |target|
   # end
 
   match do |subject|
+    subject = mock_subject(subject, target)
+    instance = subject.new({ key: 'value' })
+
+    (instance.induction & true) || begin
+      instance
+        .tap do |s|
+          @host.argument ?
+          s.send(@host.name, @host.argument) : s.send(@host.name)
+        end
+        .induction & true
+    end
   end
 
-  chain :inside do |host|
+  chain :inside do |host, &block|
+    @host = mock_host.new(host, &block)
   end
 end
