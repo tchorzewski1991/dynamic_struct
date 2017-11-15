2723ea2319a70c138d1073d2ed3764d3884786ff
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-15 21:58:43 +0100

    Expand specs for DynamicStruct#construct method

--- spec/dynamic_struct_spec.rb
+++ spec/dynamic_struct_spec.rb
@@ -30,6 +30,52 @@ RSpec.describe DynamicStruct do
       it 'expects context to return instance of DynamicStruct::Corpus' do
         expect(subject.construct.class).to be(DynamicStruct::Corpus)
       end
+
+      it 'expects to has arity of -1 as arguments are optional' do
+        expect(subject.singleton_method(:construct).arity).to eq(-1)
+      end
+
+      context 'for given arguments -' do
+        it 'expects to set arguments from block only' do
+          struct = subject.construct { |s| s.i = 'j' }
+          expect(struct.i).to eq('j')
+        end
+
+        it 'expects to set arguments from hash only' do
+          struct = subject.construct(i: 'j')
+          expect(struct.i).to eq('j')
+        end
+
+        it 'expects to set arguments from both when mixed' do
+          struct = subject.construct(i: 'j') { |s| s.k = 'l' }
+          expect([struct.i, struct.k]).to eq(%w(j l))
+        end
+
+        it 'expects to prioritize arguments from hash' do
+          struct = subject.construct(i: 'j') { |s| s.i || s.k = 'l' }
+          expect([struct.i, struct.k]).to eq(['j', nil])
+        end
+
+        it 'allows for block manipulation with public instance methods' do
+          struct = subject.construct(i: 'j') do |s|
+            s.atoms? || s.m = 'n'
+            s['k'] = 'l'
+          end
+
+          expect([struct.i, struct.k, struct.m]).to eq(['j', 'l', nil])
+        end
+
+        it 'ignores block argument without block variable' do
+          struct = subject.construct { (s.i = 'j') rescue nil }
+          expect(struct.i).to eq(nil)
+        end
+      end
+
+      context 'for missing arguments -' do
+        it 'expects to return empty instance of DynamicStruct::Corpus' do
+          expect(subject.construct.atoms?).to eq(false)
+        end
+      end
     end
   end
 end
