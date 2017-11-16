730fad30e28f0abc4e9603490fdcc7d5c4c875ac
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-11-16 19:08:43 +0100

    Tweak some descriptions for DynamicStruct module

--- spec/dynamic_struct_spec.rb
+++ spec/dynamic_struct_spec.rb
@@ -11,47 +11,42 @@ RSpec.describe DynamicStruct do
     expect(subject.class).to be(Module)
   end
 
-  describe 'constants -' do
-    it 'expects subject to define Corpus constant' do
-      expect(subject.const_defined?(:Corpus)).to eq(true)
-    end
+  it 'defines Corpus constant' do
+    expect(subject.const_defined?(:Corpus)).to eq(true)
   end
 
-  describe 'custom methods -' do
+  describe 'custom methods' do
     context '#construct -' do
-      it 'expects subject respond to context' do
+      it 'is expected subject respond to context' do
         expect(subject.respond_to?(:construct)).to be_truthy
       end
 
-      it 'expects context to be a singleton method' do
-        expect(subject.singleton_class.method_defined?(:construct)).to eq(true)
-      end
-
-      it 'expects context to return instance of DynamicStruct::Corpus' do
+      it 'returns instance of DynamicStruct::Corpus' do
         expect(subject.construct.class).to be(DynamicStruct::Corpus)
       end
 
-      it 'expects to has arity of -1 as arguments are optional' do
+      # As arguments are optional
+      it 'has arity of -1' do
         expect(subject.singleton_method(:construct).arity).to eq(-1)
       end
 
-      context 'for given arguments -' do
-        it 'expects to set arguments from block only' do
+      context 'when arguments given' do
+        it 'sets arguments from block only' do
           struct = subject.construct { |s| s.i = 'j' }
           expect(struct.i).to eq('j')
         end
 
-        it 'expects to set arguments from hash only' do
+        it 'sets arguments from hash only' do
           struct = subject.construct(i: 'j')
           expect(struct.i).to eq('j')
         end
 
-        it 'expects to set arguments from both when mixed' do
+        it 'sets arguments from both when mixed' do
           struct = subject.construct(i: 'j') { |s| s.k = 'l' }
           expect([struct.i, struct.k]).to eq(%w(j l))
         end
 
-        it 'expects to prioritize arguments from hash' do
+        it 'is expected to prioritize arguments from hash' do
           struct = subject.construct(i: 'j') { |s| s.i || s.k = 'l' }
           expect([struct.i, struct.k]).to eq(['j', nil])
         end
@@ -71,8 +66,8 @@ RSpec.describe DynamicStruct do
         end
       end
 
-      context 'for missing arguments -' do
-        it 'expects to return empty instance of DynamicStruct::Corpus' do
+      context 'when arguments missing' do
+        it 'returns empty instance of DynamicStruct::Corpus' do
           expect(subject.construct.atoms?).to eq(false)
         end
       end
